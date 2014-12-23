a6fa3e8900e7d834f37c5beacf86816e1558e56f
tekhuy <huyle20@hotmail.com>
2014-12-23 17:49:22 +0000

    attr reader and writer methods

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,25 +1,15 @@
 class Ticket
 
-  def initialize(venue, date, price)
+  attr_reader :venue, :date, :price
+  attr_writer :price 
+
+  def initialize(venue, date)
     @venue = venue
     @date = date
-    @price = price
-  end
-
-  def venue
-    @venue
-  end
-
-  def date
-    @date
   end
 
   def discount(percent)
     @price = @price * (100 - percent) / 100
-  end
-
-  def price
-    @price
   end
 
 end
