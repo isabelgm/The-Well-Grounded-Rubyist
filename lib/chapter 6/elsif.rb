e315062e78e1553fee018bac0a0db59e4ccc2bca
tekhuy <huyle20@hotmail.com>
2014-12-29 22:06:04 +0000

    controlflow

---
+++ lib/chapter 6/elsif.rb
@@ -0,0 +1,16 @@
+print "Enter an integer: "
+n = gets.to_i
+if n > 0
+  puts "Your number is positive."
+elsif n < 0
+  puts "Your number is negative."
+else
+  puts "Your number is zero"
+end
+
+x = 99
+if x <= 100
+  puts "Small number!"
+else
+  puts "Big number!"
+end
